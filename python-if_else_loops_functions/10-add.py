9b8b12a505667723d5840d3bc5c24a7ac839aa2f
angep72 <p.umunyana@alustudent>
2023-07-11 01:55:20 -0700

    ff

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-# 101-remove_char_at.py
+# 102-magic_calculation.py
 
 
-def remove_char_at(str, n):
-    """Create a copy of the string without the character at position n."""
-    if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
