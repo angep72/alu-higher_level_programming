4e0a13c4eb58f0d0593671de9127c957f7b1b752
angep72 <p.umunyana@alustudent>
2023-07-11 01:57:00 -0700

    g

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
+# 10-add.py
 
 
-def magic_calculation(a, b, c):
-    """Match bytecode provided by Holberton School."""
-    if a < b:
-        return (c)
-    if c > b:
-        return (a + b)
-    return (a*b - c)
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
