6bfb2c02d577dc172e1582690b748902847690c3
angep72 <p.umunyana@alustudent>
2023-07-17 23:20:07 -0700

    hellow

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    new_list = []
-    for item in my_list:
-        if item == search:
-            new_list.append(replace)
-        else:
-            new_list.append(item)
-    return new_list
+def element_at(my_list, idx):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    return my_list[idx]
