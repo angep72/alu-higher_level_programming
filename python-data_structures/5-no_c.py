7ce15cbd89cb8ff3f6ab927e5634e005d6aa10de
angep72 <p.umunyana@alustudent>
2023-07-17 23:39:11 -0700

    Noc

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+
+    result = []
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            result.append(char)
+    return ''.join(result)
