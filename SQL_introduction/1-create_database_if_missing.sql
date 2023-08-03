820f466fc46152b2be319759e6a53d5e2a9805be
angep72 <p.umunyana@alustudent>
2023-08-03 10:55:45 -0700

    if it doesn't exist

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,2 @@
-# creating first databases in my server
--- Creating the first database
-CREATE DATABASE hbtn_0c_0;
 -- This is a script to create a database
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
