29db4ba2632c109a0e1d8bde080a49c630eecd32
angep72 <p.umunyana@alustudent>
2023-08-04 04:42:48 -0700

    table

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
--- The script that creates the table id not null
+-- The script that creates the table id not null and it should not fail 
 CREATE TABLE IF NOT EXISTS (
 	id INT  DEFAULT 1,
 	name VARCHAR(256)
