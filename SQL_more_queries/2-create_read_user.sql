f5187aa309b8ae3b9c1630f6fe0f22c4120b41b1
angep72 <p.umunyana@alustudent>
2023-08-04 05:59:08 -0700

    read

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,10 @@
--- The script that creates the databases hbtn_0d_2 and user_0d2
---the user should have only SELECT privilege in this database
--- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
--- Grant SELECT privilege on the database to the user
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
+-- Creates the database hbtn_0d_2 and the user user_0d_2
+-- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
+CREATE DATABASE
+    IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost';
