3ab52f919f6f0356fd024a7d11aa006cfa136e7e
angep72 <p.umunyana@alustudent>
2023-08-04 00:52:33 -0700

    delete

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
---Deletes a databas;
-DROP DATABASE IF EXISTS hbtn_0c_0;
+-- This isa script to remove a database and ensuring it doesn,t fail when the database doesn't exist 
+DROP DATABASE IF EXISTS hbtn_0c_0
