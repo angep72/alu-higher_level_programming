1c47ba42d0d0706744fff0e28d7c32218a826188
angep72 <p.umunyana@alustudent>
2023-08-04 07:29:27 -0700

    delete

--- python-network_0/2-delete.sh
+++ python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends GET request and displays body of response
-curl -sL "$1"
+#sends a DELETE request to the URL passed as the first argument and displays the body of the response
+curl -sX DELETE "$1"
