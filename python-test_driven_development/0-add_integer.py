d5a92812d11f911c29f8ad0df339542c2042a41e
angep72 <p.umunyana@alustudent>
2023-08-14 04:43:47 -0700

    adding two integer

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-add_integer = __import__('0-add_integer').add_integer
+'''a function that adds 2 integers.'''
 
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
-try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
-except Exception as e:
-    print(e)
 
+def add_integer(a, b=98):
+    ''' Function that adds two integers
+    Args:
+        a : this must be either an integer or float
+        b : Must be either an integer or float, and if not provided
+            it takes the defualt value of 98
+    Returns:
+        an integer: the addition of a and b
+    '''
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
+
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    if type(b) != int:
+        raise TypeError("b must be an integer")
+
+    return a + b
