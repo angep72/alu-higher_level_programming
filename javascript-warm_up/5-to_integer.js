a62a6efa7c3bc52974530ab5ad85514e2c997a00
angep72 <p.umunyana@alustudent.com>
2023-09-29 10:06:31 +0000

    interger

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-
-const arg2 = process.argv[2];
-const parsedNumber = parseInt(arg2);
-
-if (isNaN (parsedNumber)){
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Not a number');
 } else {
-  console.log('My number: ${parsedNumber}');
+  console.log('My number:', parseInt(process.argv[2]));
 }
