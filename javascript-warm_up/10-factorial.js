f119d4f5db5caab7e2e54245943353c898a3366f
angep72 <p.umunyana@alustudent.com>
2023-09-29 14:21:23 +0000

    factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,18 @@
 #!/usr/bin/node
-function factorial (n) {
-  if (n < 0) {
-    return (-1);
-  }
-  if (n === 0 || isNaN(n)) {
-    return (1);
-  }
-  return (n * factorial(n - 1));
+
+const fact = parseInt(process.argv.slice(2)[0]);
+
+if (isNaN(fact) === true) {
+  console.log(1);
+} else {
+  const result = Factorial(fact);
+  console.log(result);
 }
 
-console.log(factorial(Number(process.argv[2])));
+function Factorial (num) {
+  if (num === 1) {
+    return 1;
+  } else {
+    return (num * Factorial(num - 1));
+  }
+}
