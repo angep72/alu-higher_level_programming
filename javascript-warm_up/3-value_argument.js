b85a1902080f5cbb730c038e96a3ee4c51674149
angep72 <p.umunyana@alustudent.com>
2023-09-29 08:26:05 +0000

    arguments

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-const firstArg = process.argv[2];
+const args = process.argv.slice(2);
 
-if (firstArg) {
-  console.log(firstArg);
-} else {
+if (args[0] === undefined) {
   console.log('No argument');
-}
+} else {
+  console.log(args[0]);
