a3b1139cdea02c92c2d74e766dd1e5695494b96d
angep72 <p.umunyana@alustudent.com>
2023-09-29 12:37:41 +0000

    multic

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+let x = process.argv[2];
+let i = 0;
+
+if (!Number.isInteger(Number(x))) {
   console.log('Missing number of occurrences');
 } else {
-  const x = Number(process.argv[2]);
-  let i = 0;
+  x = Number(x);
   while (i < x) {
     console.log('C is fun');
     i++;
