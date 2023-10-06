bb26701d0ce11c1d28e86d1c54d865481ee99da1
angep72 <p.umunyana@alustudent.com>
2023-10-06 08:17:38 +0000

    readme.js

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,4 @@
+#!/usr/bin/nodconst fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content){
+console.log(error || content);
+});
---
+++ javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+This document is a cheatsheet for JavaScript you will frequently encounter in modern projects and most contemporary sample code.
